b0fb36293ccde14bd1da8574153b9c6453fb3b36
fgma <fgma@users.noreply.github.com>
2019-04-29 22:07:25 +0200

    - gox-build.sh support for version number from travis

--- gox-build.sh
+++ gox-build.sh
@@ -4,10 +4,21 @@ set -e
 #
 # build releases using https://github.com/mitchellh/gox
 #
-gox -ldflags "-s -w -X github.com/fgma/rester/cmd.versionRevision=`git rev-parse --short HEAD`" -rebuild
 
-rm -f ./rester_*_*.bz2
-for f in ./rester_*_* ; do
+OUTPUT_DIR=release
+OUTPUT_TEMPLATE="$OUTPUT_DIR/{{.Dir}}_{{.OS}}_{{.Arch}}"
+
+if [ -n $1 ]; then
+	OUTPUT_TEMPLATE="$OUTPUT_DIR/{{.Dir}}_$1_{{.OS}}_{{.Arch}}"
+fi
+
+mkdir $OUTPUT_DIR
+gox -ldflags "-s -w -X github.com/fgma/rester/cmd.versionRevision=`git rev-parse --short HEAD`" \
+  -output ${OUTPUT_TEMPLATE}
+  -rebuild
+
+rm -f ${OUTPUT_DIR}/rester_*_*.bz2
+for f in ${OUTPUT_DIR}/rester_*_* ; do
     echo "Compressing $f"
     bzip2 -f "$f"
 done
