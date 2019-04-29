acb2aafb40ac6e9c92de812fcf3fb4befcbfd2db
fgma <30936930+fgma@users.noreply.github.com>
2019-04-29 22:49:18 +0200

    improved travis configuration (#2)
    * travis: build only against stable golang
    
    * set stable to latest
    
    * build only against specific golang versions
    
    * improved gox-build.sh for travis builds
    
    * test travis deploy
    
    * travis: correct globbing syntax
    
    * travis: deploy in test branch
    
    * - gox-build.sh support for version number from travis
    
    * typo in gox-build.sh
    
    * specify output folder in gox-build.sh
    
    * travis: deploy only on tagged master

--- gox-build.sh
+++ gox-build.sh
@@ -4,10 +4,25 @@ set -e
 #
 # build releases using https://github.com/mitchellh/gox
 #
-gox -ldflags "-s -w -X github.com/fgma/rester/cmd.versionRevision=`git rev-parse --short HEAD`" -rebuild
 
-rm ./rester_*_*.bz2
-for f in ./rester_*_* ; do
+OUTPUT_DIR=release
+OUTPUT_TEMPLATE="$OUTPUT_DIR/{{.Dir}}_{{.OS}}_{{.Arch}}"
+
+if [ -n $1 ]; then
+	OUTPUT_DIR="$1"
+fi
+
+if [ -n $2 ]; then
+	OUTPUT_TEMPLATE="$OUTPUT_DIR/{{.Dir}}_$2_{{.OS}}_{{.Arch}}"
+fi
+
+mkdir $OUTPUT_DIR
+gox -ldflags "-s -w -X github.com/fgma/rester/cmd.versionRevision=`git rev-parse --short HEAD`" \
+  -output ${OUTPUT_TEMPLATE} \
+  -rebuild
+
+rm -f ${OUTPUT_DIR}/rester_*_*.bz2
+for f in ${OUTPUT_DIR}/rester_*_* ; do
     echo "Compressing $f"
     bzip2 -f "$f"
 done
